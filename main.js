7ec04996ac9daf7bed22712a63d0bc1affecad0c
Samuel <sjoseph1848@gmail.com>
2019-02-06 22:50:55 -0600

    Updated UI elements with javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+//Let's grab the UI elements
+const form = document.querySelector('#packing-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-packing');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#packing');
